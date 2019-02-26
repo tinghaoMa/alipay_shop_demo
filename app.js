a6c3fae14eb5eea32b4902a3cba7c39e1a1f9afc
tinghao.ma <tinghao.ma@ele.me>
2019-02-26 15:15:48 +0800

    test

--- app.js
+++ app.js
@@ -32,4 +32,7 @@ App({
       });
     });
   },
+	 onLaunch(options) {
+		console.log('onLaunch')
+  },
 });
--- pages/test/test.js
+++ pages/test/test.js
@@ -1,35 +1,55 @@
 const app = getApp();
 
 Page({
-  data: {},
-
-  onLoad() {
-    app.getUserInfo().then(
-      user => {
-        this.setData({
-          user,
-        });
-      },
-      () => {
-        // 获取用户信息失败
-      }
-    );
+  data: {
+    text: 'test',
+    array: [{text: 'a'}],
+    object: {
+      text: 'blue'
+    },
+		name: 'taobao',
+		a: {
+      b: [1,2,3,4]
+    },
+		items: [1, 2, 3],
+		view:'APP'
   },
 
-  onShow() {
-    this.setData({ todos: app.todos });
+	onLoad(){
+    this.$spliceData({ 'a.b': [1, 0, 5, 6] })
   },
 
-  onTodoChanged(e) {
-    const checkedTodos = e.detail.value;
-    app.todos = app.todos.map(todo => ({
-      ...todo,
-      completed: checkedTodos.indexOf(todo.text) > -1,
-    }));
-    this.setData({ todos: app.todos });
+  changeTitle() {
+    // 错误！不要直接去修改 data 里的数据
+    // this.data.text = 'changed data'  
+    
+    // 正确
+    this.setData({
+      text: 'ha'
+    })
   },
-
-  addTodo() {
-    my.navigateTo({ url: '../add-todo/add-todo' });
+  changeArray() {
+    // 可以直接使用数据路径来修改数据
+    this.setData({
+      'array[0].text':'b'
+    })
   },
-});
+  changePlanetColor(){
+    this.setData({
+      'object.text': 'red'
+    });
+  },
+  addNewKey() {
+    this.setData({
+      'newField.text': 'c'
+    })
+  },
+  changeName() {
+    this.setData({
+      name: 'alipay',
+    }, () => { // 接受传递回调函数
+      console.log(this) // this 当前页面实例
+      this.setData({ name: this.data.name + ', ' + 'welcome!'})
+    })
+  },
+})
